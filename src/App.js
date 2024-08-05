6b780501874707a5864944c261218dc9ac01ad3a
Jishnu M <mjihnu990@gmail.com>
2024-08-05 14:46:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React from 'react'
+import NavBar from './Components/NavBar'
+import Banner from './Components/Banner/Banner'
+import Row from './Components/Row/Row'
+import { originals, ActionMovies, Romance, HorrorMovies, Documentaries, Comedy } from './urls'
+import Footer from './Components/Footer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <Banner />
+      <Row title='Netflix Originals' Notitle url={originals} />
+      <Row title='Action' isSamll url={ActionMovies} />
+      <Row title='Romance' url={Romance} />
+      <Row title='Horror' url={HorrorMovies} />
+      <Row title='Comedy' url={Comedy} />
+      <Row title='Documentaries' url={Documentaries} />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+// import React, { useState } from 'react'
+// import axios from 'axios'
+
+// function App() {
+//   const [state, setState] = useState([])
+//   return (
+//     <div> <button onClick={() => {
+//       axios.get('https://jsonplaceholder.typicode.com/posts').then((respones) => {
+//         console.log(respones.data);
+//         setState(respones.data)
+//       })
+//     }}>Click</button>
+
+//       {state.map((obj, index) => {
+//         return (
+//           <div className="">
+//             <h1>{index + 1}</h1>
+//             <h4>{obj.title}</h4>
+//             <p>{obj.body}</p>
+//           </div>
+//         )
+//       })}
+
+//     </div>
+//   )
+// }
+
+// export default App
